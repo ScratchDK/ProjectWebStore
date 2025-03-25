72842ed92639d1deaf9d34dec619f0af21c94c35
Дмитрий Коваль <bloodpaperace@mail.ru>
2025-03-25 14:09:14 +0300

    добавлен скрипт и стили для открытия карточки отдельного товара в модальном окне. обавлен базовый щаблон.

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .forms import ContactForm
 from .models import Product
@@ -5,9 +6,16 @@ from .models import Product
 
 def home(request):
     last_five_products = Product.objects.order_by('-created_at')[:5]
+    all_products = Product.objects.all()
+
+    paginator = Paginator(all_products, 8)  # Ограничиваем до 20 товаров на странице
+
+    page_number = request.GET.get('page')  # Получаем номер страницы из GET параметров
+    page_obj = paginator.get_page(page_number)  # Получаем нужную страницу
+
     for product in last_five_products:
         print(product)
-    return render(request, "home.html", {"products": last_five_products})
+    return render(request, "home.html", {"products": page_obj})
 
 
 def contacts(request):
